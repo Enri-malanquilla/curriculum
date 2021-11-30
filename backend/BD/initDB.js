27799641de08823e8f1aaf00297310eca54b3ace
Enri-malanquilla <enri.sl.malanquilla@gmail.com>
2021-11-30 13:02:12 +0100

    creada base de datos

--- backend/BD/initDB.js
+++ backend/BD/initDB.js
@@ -1,4 +1,12 @@
 const getDB = require('./getDB');
+require('dotenv').config();
+const { USUARIO, CONTRASENA } = process.env;
+const faker = require('faker/locale/es');
+//formato fecha
+const { format } = require('date-fns');
+function formatDate(date) {
+  return format(date, 'yyyy-MM-dd HH:mm:ss');
+}
 
 async function main() {
   let connection;
@@ -8,10 +16,10 @@ async function main() {
 
     await connection.query('DROP TABLE IF EXISTS fotos');
     await connection.query('DROP TABLE IF EXISTS usuario');
-    await connection.query('DROP TABLE IF EXISTS instituciones');
     await connection.query('DROP TABLE IF EXISTS experiencia');
     await connection.query('DROP TABLE IF EXISTS estudios');
     await connection.query('DROP TABLE IF EXISTS otros');
+    await connection.query('DROP TABLE IF EXISTS instituciones');
 
     console.log('tablas borradas, comienza el reinicio');
 
@@ -20,6 +28,7 @@ async function main() {
       `
         CREATE TABLE usuario (
             email VARCHAR(100) UNIQUE NOT NULL,
+            alias VARCHAR(512) NOT NULL,
             password VARCHAR(512) NOT NULL,
             nombre VARCHAR(100),
             apellidos VARCHAR(100),
@@ -46,6 +55,140 @@ async function main() {
         `
     );
     console.log('Iniciada tabla instituciones');
+
+    await connection.query(`
+    CREATE TABLE experiencia (
+        id INT PRIMARY KEY AUTO_INCREMENT ,
+        idEmpresa INT NOT NULL,
+        FOREIGN KEY (idEmpresa) REFERENCES instituciones(id),
+        puesto VARCHAR(100) NOT NULL,
+        descripcion TEXT,
+        fechaInicio DATETIME NOT NULL,
+        fechaFinal DATETIME,
+        actual BOOLEAN DEFAULT false
+    )
+    `);
+    console.log(`Iniciada tabla experiencia`);
+
+    await connection.query(`
+    CREATE TABLE estudios(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        idEmpresa INT NOT NULL,
+        FOREIGN KEY (idEmpresa) REFERENCES instituciones(id),
+        nota INT,
+        apto BOOLEAN DEFAULT true NOT NULL,
+        temario TEXT
+        
+
+    )
+    `);
+
+    console.log('Iniciada tabla estudios');
+
+    await connection.query(`
+    CREATE TABLE otros (
+        id INT PRIMARY KEY AUTO_INCREMENT PRIMARY KEY,
+        titulo VARCHAR(100) NOT NULL,
+        descripcion TEXT,
+        web BOOLEAN DEFAULT false,
+        enlace TEXT,
+        enlace2 TEXT,
+        enlace3 TEXT
+
+
+        
+    )
+    `);
+
+    console.log('Iniciada tabla otros');
+
+    await connection.query(`
+    CREATE TABLE fotos (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        idEmpresa INT NOT NULL,
+        FOREIGN KEY (idEmpresa) REFERENCES instituciones(id),
+        idOtros INT NOT NULL,
+        FOREIGN KEY (idOtros) REFERENCES otros(id),
+        name VARCHAR(50) NOT NULL
+
+    )
+    `);
+    console.log('Iniciada tabla fotos');
+
+    /*
+    #########################
+    ###### CREAR USUARIO ####
+    #########################
+    */
+
+    await connection.query(`
+    INSERT INTO usuario (email, alias, password, nombre, apellidos, github, linkdin, puesto)
+    VALUES (
+        "enri.sl.malanquilla@gmail.com",
+        SHA2("${USUARIO}", 512),
+        SHA2("${CONTRASENA}", 512),
+        "enrique",
+        "sanchez llamas",
+        "https://github.com/Enri-malanquilla",
+        "https://www.linkedin.com/in/enriquesanchezmalanquilla/",
+        "Desarrollador web"
+    )
+    `);
+    console.log('Usuario creado');
+
+    /*
+    ###############################
+    ###### CREAR PRIMERA COLUMNA###
+    ###############################
+    */
+
+    for (let i = 1; i < 4; i++) {
+      const nombreCompania = faker.company.companyName();
+      const puesto = faker.commerce.productName();
+      const descripcion = faker.commerce.productDescription();
+      const nota = Math.random() * 9;
+      const url = faker.internet.url();
+
+      await connection.query(`
+        INSERT INTO instituciones ( nombre)
+        VALUES (
+            "${nombreCompania}"
+        )
+        `);
+      console.log('Creada institución');
+
+      await connection.query(`
+        INSERT INTO experiencia (idEmpresa, puesto, descripcion, fechaInicio)
+        VALUES (
+            '${i}',
+            '${puesto}',
+            '${descripcion}',
+            '${formatDate(new Date())}'
+        )
+        `);
+      console.log('Creado experiencia');
+
+      await connection.query(`
+        INSERT INTO estudios (idEmpresa, nota, temario)
+        VALUES (
+            "${i}",
+            "${nota}",
+            "${descripcion}"
+        )
+        `);
+      console.log('Creado estudios');
+
+      await connection.query(`
+        INSERT INTO otros (titulo, descripcion, enlace)
+        VALUES(
+            "${puesto}",
+            "${descripcion}",
+            "${url}"
+        )
+        `);
+
+      console.log('Creado estudios');
+    }
   } catch (error) {
     console.error(error.message);
   } finally {
---
+++ backend/README.md
@@ -0,0 +1,3 @@
+## INICIO BASE DE DATOS
+
+1. Tabla "otros" => en enlace que sea obligatorio si web es true.
